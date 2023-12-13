d92f0295a258e28653a75311fcf363f46ca04954
ddjwajww <127021017+ddjwajww@users.noreply.github.com>
2023-12-13 10:14:24 +0300

    x

---
+++ Login/Login/Repositories/RepositoryBase.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace Login.Repositories
+{
+    public abstract class RepositoryBase
+    {
+        private readonly string _connectionString;
+        public RepositoryBase()
+        {
+            _connectionString = "Server = localhost\\SQLEXPRESS; Database = MVVMLoginDb; TrustServerCertificate=true; Trusted_Connection = True;";
+        }
+
+        protected SqlConnection GetConnection()
+        {
+            return new SqlConnection( _connectionString );
+        }
+    }
+}
